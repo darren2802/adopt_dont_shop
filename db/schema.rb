e9b26473ca12cb80f28c56f2eb9a928412b27995
Darren Campbell <darren2802@gmail.com>
2019-11-25 09:37:45 -0700

    Add description column to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191125135417) do
+ActiveRecord::Schema.define(version: 20191125162152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20191125135417) do
     t.bigint "shelter_id"
     t.boolean "adoptable"
     t.string "breed"
+    t.text "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
